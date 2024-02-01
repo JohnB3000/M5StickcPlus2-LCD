33d1598ff630a107b7a24874cb83e26d1202b592
JohnB3000 <138237233+JohnB3000@users.noreply.github.com>
2024-02-01 17:28:57 +0000

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,23 @@
 #include <Arduino.h>
-#include <FastLED.h>
 
-// LED 1
-#define NUM_LEDS 20
-#define LED_PIN 26
-CRGB leds[NUM_LEDS];
-
-//hue values
-uint8_t ceiling_hue = 0; // LED 1
-
-//saturation values
-uint8_t ceiling_sat = 255; //LED 1
-
-//value values
-uint8_t ceiling_val = 130; // LED 1
+#include "M5StickCPlus2.h"
 
 void setup() {
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);         // LED 1
-
-  FastLED.setBrightness(127); // 0 - 255
-  FastLED.clear();
-  FastLED.show();
+    auto cfg = M5.config();
+    StickCP2.begin(cfg);
+    StickCP2.Display.setRotation(1);
+    StickCP2.Display.setTextColor(GREEN);
+    StickCP2.Display.setTextDatum(middle_center);
+    StickCP2.Display.setTextFont(&fonts::Orbitron_Light_24);
+    StickCP2.Display.setTextSize(1);
+    StickCP2.Display.drawString("Power LED", StickCP2.Display.width() / 2,
+                                StickCP2.Display.height() / 2);
 }
 
 void loop() {
-
-  // LED 1
-  for ( int i=0; i < 144; i++)
-    leds[i].setHSV(ceiling_hue,ceiling_sat,ceiling_val);
-    FastLED.show();
-
+    // inside power red led control
+    StickCP2.Power.setLed(1);
+    delay(1000);
+    StickCP2.Power.setLed(0);
+    delay(1000);
 }
