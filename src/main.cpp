b35a14516ff125a191e8c02945f0bd24abcab6c7
John Brzoza <john@johnb.cc>
2024-02-01 21:07:05 +0000

    clock

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,16 @@ void setup() {
     StickCP2.begin(cfg);
     StickCP2.Display.setRotation(1);
     StickCP2.Display.setTextColor(GREEN);
-    StickCP2.Display.setTextDatum(middle_center);
     StickCP2.Display.setTextFont(&fonts::Orbitron_Light_24);
-    StickCP2.Display.setTextSize(1);
-    StickCP2.Display.drawString("Power LED", StickCP2.Display.width() / 2,
-                                StickCP2.Display.height() / 2);
+    StickCP2.Display.setTextSize(1.5); 
 }
 
 void loop() {
-    // inside power red led control
-    StickCP2.Power.setLed(1);
-    delay(1000);
-    StickCP2.Power.setLed(0);
-    delay(1000);
+    StickCP2.Display.clear();
+    StickCP2.Display.setCursor(35, 40);
+    auto dt = StickCP2.Rtc.getDateTime();
+    StickCP2.Display.printf("%02d:%02d:%02d",
+                            dt.time.hours, dt.time.minutes,
+                            dt.time.seconds);
+    sleep(1);
 }
